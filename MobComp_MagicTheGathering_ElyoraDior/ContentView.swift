777d72aee6a4cf0b19b6e6dc59e5e2513e528d8e
Elyora Dior <elyoradior01@stduent.ciputra.ac.id>
2023-11-10 08:00:19 +0700

    Initial Commit

---
+++ MobComp_MagicTheGathering_ElyoraDior/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MobComp_MagicTheGathering_ElyoraDior
+//
+//  Created by MacBook Pro on 10/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MobComp_MagicTheGathering_ElyoraDior/MobComp_MagicTheGathering_ElyoraDiorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobComp_MagicTheGathering_ElyoraDiorApp.swift
+//  MobComp_MagicTheGathering_ElyoraDior
+//
+//  Created by MacBook Pro on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MobComp_MagicTheGathering_ElyoraDiorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
